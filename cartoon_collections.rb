03fd2be8b131f81f771d0941ae0a5cc7cbdd1317
Christian Perez <perezchristian1012@gmail.com>
2019-07-26 20:33:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,12 +13,8 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  if array.length.all? > 4
-    return true
-  else
-    return false
+  if array.any? { |calls| calls.length > 4 }
   end
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
