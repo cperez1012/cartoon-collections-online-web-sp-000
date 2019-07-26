d90f1d64ba57c32f0b3e9003a6c5da9ab8468af4
Christian Perez <perezchristian1012@gmail.com>
2019-07-26 20:34:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  if array.any? { |calls| calls.length > 4 }
+  array.any? { |calls| calls.length > 4 }
   end
 
 def find_the_cheese# code an argument here
