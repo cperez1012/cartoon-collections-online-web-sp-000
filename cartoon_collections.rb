0f0b530e9670f9131777d8ff9342ac0eb94d60d8
Christian Perez <perezchristian1012@gmail.com>
2019-07-26 20:07:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,7 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here
   array.map { |i| puts "i.capitalize" }
+  Array.upshift "!"
 end
 
 def long_planeteer_calls# code an argument here
